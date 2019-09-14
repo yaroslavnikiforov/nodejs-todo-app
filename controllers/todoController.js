fe1ff1f0031d369a7351014f4b83d955523461b6
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-09-14 14:07:34 +0300

    Connect mongodb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,31 @@
 module.exports = function todoController(app) {
   const bodyParser = require("body-parser");
+  const mongoose = require("mongoose");
+
+  // connect to the database
+  mongoose.connect(
+    "mongodb+srv://yaroslav:88888888zaq1xsw2cde3vfr4@cluster0-kkm1s.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+
+  // create a schema
+
+  const todoSchema = mongoose.Schema({
+    item: String
+  });
+
+  const Todo = mongoose.model("Todo", todoSchema);
+
+  const itemOne = Todo({ item: "buy flowers" }).save(err => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("item saved");
+    }
+  });
 
   let data = [
     { item: "get milk" },
